bd4718ef7cfeedfdd3058de87ec5ad3700a91a1d
Ioannis Asmanis <asmanis.ioannis@gmail.com>
2019-10-18 22:59:47 +0200

    Fixed core functionality, still need IO and calib semantics check

--- img_inspector.cpp
+++ img_inspector.cpp
@@ -11,20 +11,35 @@
 #define TAR_WIDTH 512
 #define TAR_HEIGHT 512
 
-#define SYMMETRIC_CROPPING 0
-#define ASYMMETRIC_CROPPING 1
-#define CROPPING_POLICY SYMMETRIC_CROPPING     // Change according to your logic
+//#define SYMMETRIC_CROPPING 0
+//#define ASYMMETRIC_CROPPING 1
+//#define CROPPING_POLICY SYMMETRIC_CROPPING     // Change according to your logic
 
 // File opening definitions
-#define DATA_ROOT "/home/galar/Documents/morocco_SFR/merzouga/merzouga-minnie-trajectory21-1/front_cam/"
-#define LEFT_IMG_PATH "raw/left/"
-#define RIGHT_IMG_PATH "raw/right/"
-#define PIC_FILE_IDX_LIM 0
-#define IMG_FILE_FORMAT "%05d.pgm"
+//#define DATA_ROOT "/home/galar/Documents/morocco_SFR/merzouga/merzouga-minnie-trajectory21-1/front_cam/"
+#define DATA_ROOT "/home/jackfrost/Documents/ESA/sfr_morocco/extra_files/"
+//#define LEFT_IMG_PATH "raw/left/"
+//#define RIGHT_IMG_PATH "raw/right/"
+#define LEFT_IMG_PATH ""
+#define RIGHT_IMG_PATH ""
+#define LEFT_MARK ""
+//#define RIGHT_MARK ""
+#define RIGHT_MARK "r"
+#define IMG_FILE_FORMAT "%05d%s.pgm"
 #define CALIB_FILE "frontcam-calibration.yaml"
 #define IMG_DIR_START_IDX 2
 #define IMG_DIR_END_IDX 2
 
+// Output definitions
+#define OUTPUT_IMG_DIR "/home/jackfrost/Documents/ESA/sfr_morocco/output/"
+#define OUTPUT_LEFT_IMG_PATH "left/"
+#define OUTPUT_RIGHT_IMG_PATH "right/"
+#define OUTPUT_LEFT_MARK ""
+#define OUTPUT_RIGHT_MARK ""
+#define OUTPUT_START_IDX 0
+#define OUTPUT_IMG_FILE_FORMAT IMG_FILE_FORMAT
+#define OUTPUT_CALIB_DIR "/home/jackfrost/Documents/ESA/sfr_morocco/output/"
+#define OUTPUT_CALIB_FILE "frontcam-calibration_512x512_rectified.yaml"
 
 // Utilities
 #define MAX_STR_LEN 128
@@ -49,9 +64,9 @@ Mat getImgMat(int index, bool isLeft=true, bool isGrayscale=true)
     strcpy(file_template, DATA_ROOT);
     strcat(file_template, ((isLeft) ? LEFT_IMG_PATH : RIGHT_IMG_PATH));
     strcat(file_template, IMG_FILE_FORMAT);
-    sprintf(fname, file_template, index);
+    sprintf(fname, file_template, index, ((isLeft) ? LEFT_MARK : RIGHT_MARK));
 
-    Mat res = imread(fname, ((isGrayscale)?IMREAD_GRAYSCALE:IMREAD_COLOR));
+    Mat res = imread(fname, ((isGrayscale) ? IMREAD_GRAYSCALE : IMREAD_COLOR));
     if (!res.data) {
         cerr << "Error loading image file: "
             << fname
@@ -91,6 +106,32 @@ Mat getImgMat(int index, bool isLeft=true, bool isGrayscale=true)
 
 //}
 
+
+/**
+* Returns a reasonable size by minimally editing the size of the original
+* image to match the target aspect ratio
+* */
+Size getIntermediateSize()
+{
+	double aspect_ratio_orig = ((double) ORIG_WIDTH) / ORIG_HEIGHT;
+	double aspect_ratio_tar = ((double) TAR_WIDTH) / TAR_HEIGHT;
+
+	double width = ORIG_WIDTH, height = ORIG_HEIGHT;
+
+	// Only edit the dimension that is largest
+	if (width > height) {
+		width = height * aspect_ratio_tar;
+	} else {
+		height = width / aspect_ratio_tar;
+	}
+
+	return Size(cvRound(width), cvRound(height));
+}
+
+
+/**
+* Computes matrices for the image transformations
+* */
 void getFinalMatrices(Mat& p1, Mat&p2, Mat& out_left_map1, Mat& out_left_map2, Mat& out_right_map1, Mat& out_right_map2)
 {
     // Read original calibration file
@@ -115,13 +156,13 @@ void getFinalMatrices(Mat& p1, Mat&p2, Mat& out_left_map1, Mat& out_left_map2, M
     cout << t << endl;
 #endif
 
-    // Get new matrices after rectification and resizing
-    Size final_sz(TAR_WIDTH, TAR_HEIGHT);
+    // Get matrices and maps for rectification and aspect ratio correction
+	Size final_sz = getIntermediateSize();
     Mat r1, r2, q;
     stereoRectify(
             c1, d1, c2, d2, Size(ORIG_WIDTH, ORIG_HEIGHT), r, t, // inputs
             r1, r2, p1, p2, q,   // outputs
-            CALIB_ZERO_DISPARITY, -1, final_sz   // defaults
+            CALIB_ZERO_DISPARITY, 0, final_sz   // defaults -> use alpha=0 to avoid black edges in the output image
     );
 
     initUndistortRectifyMap(
@@ -136,32 +177,61 @@ void getFinalMatrices(Mat& p1, Mat&p2, Mat& out_left_map1, Mat& out_left_map2, M
 }
 
 /**
- * Create a window and display an image in it, then wait for user action
+ * Create a window and display an image in it, then optionally wait for user action
  * */
-void displayImage(Mat input_img, int index, bool isOriginal=true)
+void displayImage(Mat input_img, int index, bool isOriginal, bool waitAfterDisplay=true)
 {
     char win_name[MAX_STR_LEN];
     sprintf(win_name, ((isOriginal) ? TITLE_ORIG_FORMAT : TITLE_PROC_FORMAT), index);
     namedWindow(win_name, CV_WINDOW_AUTOSIZE);
     imshow(win_name, input_img);
-    waitKey();
+    if (waitAfterDisplay) {
+		waitKey();
+	}
 }
 
 
+void printImgToFile(Mat img, int idx, bool isLeft)
+{
+	//TODO: complete this using the OUTPUT_ macros
+	return;
+}
+
+
+/**
+* Function that accepts an index and the necessary matrices to
+* transform a left-right stereo image pair to the appropriate
+* format and store the results in the corresponding output
+* image files
+* */
 void fixSinglePair(int idx, Mat lm1, Mat lm2, Mat rm1, Mat rm2)
 {
+	// Rectify images and simultaneously fix their aspect ratios
     Mat left_img = getImgMat(idx), right_img = getImgMat(idx, false);
     Mat left_new, right_new;
     remap(left_img, left_new, lm1, lm2, INTER_LINEAR);
     remap(right_img, right_new, rm1, rm2, INTER_LINEAR);
 
-    displayImage(left_img, idx);
-    displayImage(left_new, idx, false);
+	// Resize them to their final size, using INTER_AREA interpolation to avoid Moire effects
+	Mat left_final, right_final;
+	resize(left_new, left_final, Size(TAR_WIDTH, TAR_HEIGHT), 0, 0, INTER_AREA);
+	resize(right_new, right_final, Size(TAR_WIDTH, TAR_HEIGHT), 0, 0, INTER_AREA);
 
-    //TODO write new images to files
+#ifdef DEBUG
+    displayImage(left_img, idx, true, false);
+    displayImage(left_final, idx, false);
+#endif
+
+    // Write images to a file
+	printImgToFile(left_final, idx, true);
+	printImgToFile(right_final, idx, false);
 }
 
 
+/**
+* This function loops over the image directories and fixes the stereo pairs
+* one at a time, storing the results in the corresponding output directories
+* */
 void loopOverImageDirectory(Mat left_map_1, Mat left_map_2, Mat right_map_1, Mat right_map_2)
 {
     for (int i=IMG_DIR_START_IDX; i<=IMG_DIR_END_IDX; i++) {
@@ -182,6 +252,9 @@ int main(int argc, char **argv)
     cout << p1 << endl;
     cout << p2 << endl;
 #endif
+
+	// TODO Figure out how this can be automated -> REMEMBER TO RESCALE THE PROJECTION MATRICES DUE TO resize()
+	//storeJointCalibrationFile(p1, p2, r, t);
 
     loopOverImageDirectory(lm1, lm2, rm1, rm2);
 
